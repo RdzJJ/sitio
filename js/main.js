050d38516b7d9654f9ca5c969cdf5abe3e88fc29
RdzJJ <july25rodriguezjimenez2509@gmail.com>
2024-07-26 07:13:05 -0500

    configuracion basica de html y js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+window.onload=init;
+
+function init() {
+    alert("Este es un mensaje de prueba");
+}
+
+let contenedor;
+window.onload=init;
+
+function init() {
+    contenedor=document.getElementById("contenedor");
+}
