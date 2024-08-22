a2aa1efad07c2e6353c0d49ef8c9e5d46a048aec
Mariana <osoriorojasmariana@gmail.com>
2024-08-22 17:46:25 -0500

    cambios

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,9 +1,20 @@
-    // darkmode.js
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
     const darkModeToggle = document.querySelector('.dark-mode-toggle');
     const body = document.body;
 
-    darkModeToggle.addEventListener('click', function () {
+    // Verificar si hay preferencia guardada en localStorage
+    if (localStorage.getItem('dark-mode') === 'enabled') {
+        body.classList.add('dark-mode');
+    }
+
+    darkModeToggle.addEventListener('click', () => {
         body.classList.toggle('dark-mode');
+
+        // Guardar la preferencia en localStorage
+        if (body.classList.contains('dark-mode')) {
+            localStorage.setItem('dark-mode', 'enabled');
+        } else {
+            localStorage.setItem('dark-mode', 'disabled');
+        }
     });
 });
